1d827e10d7138a9640cc4ff3e549a024540c3ecf
runnerle <Runnerle@googlemail.com>
2013-12-08 23:22:42 +0000

    user model changes

--- server/models/user.rb
+++ server/models/user.rb
@@ -1,6 +1,7 @@
 class User < Sequel::Model(:user)
     plugin :validation_helpers
 
+    # relations to other tables
     one_to_many :notes,    :key => :id_user, :class => :Notes
     one_to_many :contacts, :key => :id_user, :class => :Contacts
     one_to_many :calendar, :key => :id_user, :class => :Calendar
@@ -15,6 +16,9 @@ class User < Sequel::Model(:user)
         self.register ||= Time.new 
     end
 
+    # check given login credentials.
+    # returns the user if data matches
+    # and false otherwise.
     def self.authenticate( email, password )
         login = User.find( :email => email, 
                           :password=> password,
@@ -22,6 +26,9 @@ class User < Sequel::Model(:user)
         login.nil?? false : login
     end
 
+    # returns the new registred user or
+    # false if the registration failed.
+    # This can happen if the email is already in use.
     def self.register( email, password )
         registration = self.new( :email => email,
                                 :password => password,
@@ -29,11 +36,15 @@ class User < Sequel::Model(:user)
         registration.valid?? registration.save : false 
     end
 
+    # a user has to be approved to use the service.
     def approve!
         self.approved = true
         self.save
     end
 
+    # if a user should not be accepted, this
+    # function deletes that user unless he is
+    # already approved.
     def reject!
         return false if self.approved == true
         self.destroy
