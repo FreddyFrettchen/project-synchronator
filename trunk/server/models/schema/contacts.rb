59282cd400268d170c3e6f7739aa18862d75bffb
Runnerle@googlemail.com <Runnerle@googlemail.com@6457d801-3e53-91b5-1c28-a1f0f58c1245>
2013-12-09 16:33:50 +0000

    deleted files that are no longer needed

--- trunk/server/models/schema/contacts.rb
+++
@@ -1,6 +0,0 @@
-DB.create_table? :contacts do
-	primary_key :id
-    Integer     :id_user
-    Text        :data
-	DateTime 	:last_sync
-end
--- trunk/server/models/schema/notes.rb
+++
@@ -1,6 +0,0 @@
-DB.create_table? :notes do
-	primary_key :id
-    Integer     :id_user
-    Text        :data
-	DateTime 	:last_sync
-end
