5cc3166e51fd4323d8226fd0f67b2519eeabf1cf
Runnerle@googlemail.com <Runnerle@googlemail.com@6457d801-3e53-91b5-1c28-a1f0f58c1245>
2013-12-10 17:55:21 +0000

    forgot yet another file

---
+++ trunk/server/application/models/data.rb
@@ -0,0 +1,28 @@
+['Calendar', 'Contacts', 'Notes'].each do |model|
+    table = Sequel::Model(model.downcase.to_sym)
+    Object.const_set(model, Class.new(table){
+        plugin :validation_helpers
+
+        #@override
+        def validate
+            super
+            validates_presence :data
+        end
+
+        #@override
+        def before_update
+            self.last_update = Time.new
+        end
+
+        #@override
+        def before_save
+            self.last_update = Time.new
+        end
+
+        # Returns a hash containing all fields,
+        # that should be send to the client.
+        def public_values
+            { 'id' => self.id, 'data' => self.data }
+        end
+    })
+end
