3ab726481db87305464499b63e65626b1b177957
Runnerle@googlemail.com <Runnerle@googlemail.com@6457d801-3e53-91b5-1c28-a1f0f58c1245>
2013-12-11 18:14:09 +0000

    route tests

---
+++ trunk/server/application/test/test_data_route.rb
@@ -0,0 +1,93 @@
+require_relative '../server'
+
+class TestDataRoute < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    ServerHandler
+  end
+
+  def test_add_data
+    credentials = {"email" => "add@mail.com", "password" => "dpd"}
+    post '/user/register', credentials
+    User.find(:email => credentials["email"]).approve!
+
+    # set some data to be set
+    credentials["data"] = "TESTDATA"
+    %w{calendar contact note}.each do |route|
+        post "/data/add/#{route}", credentials
+        assert_equal 200, last_response.status
+    end
+  end
+  
+  def test_add_data_fail
+    credentials = {"email" => "addfail@mail.com", "password" => "dpd"}
+    post '/user/register', credentials
+    User.find(:email => credentials["email"]).approve!
+
+    # omg data is not set, this is invalid! :)
+    %w{calendar contact note}.each do |route|
+        post "/data/add/#{route}", credentials
+        assert_equal 304 , last_response.status
+    end
+  end
+  
+  def test_add_data_fail_wrong_credentials
+    credentials = {"email" => "addfailcred@mail.com", "password" => "dpd"}
+    post '/user/register', credentials
+    User.find(:email => credentials["email"]).approve!
+   
+    # now data is set, but creds are wrong, oh noes.
+    credentials["data"] = "TESTDATA"
+    credentials["password"] = "thewrongone"
+    %w{calendar contact note}.each do |route|
+        post "/data/add/#{route}", credentials
+        assert_equal 403 , last_response.status
+    end
+  end
+  
+  def test_get_data
+    credentials = {"email" => "get@mail.com", "password" => "dpd"}
+    post '/user/register', credentials
+    User.find(:email => credentials["email"]).approve!
+
+    # set some data to be set
+    credentials["data"] = "TESTDATA"
+    %w{calendar contact note}.each do |route|
+        post "/data/add/#{route}", credentials
+    end
+
+    # now get this data!
+    %w{calendar contacts notes}.each do |route|
+        post "/data/get/#{route}", credentials
+        assert_equal 200 , last_response.status
+        assert_equal credentials["data"], JSON.parse(last_response.body).first["data"]
+    end
+  end
+
+  def test_update_data
+#    credentials = {"email" => "update@mail.com", "password" => "dpd"}
+#    post '/user/register', credentials
+#    User.find(:email => credentials["email"]).approve!
+#
+#    # set some data to be set
+#    credentials["data"] = "TESTDATA"
+#    %w{calendar contact note}.each do |route|
+#        post "/data/add/#{route}", credentials
+#    end
+#
+#    credentials["data"] = "UPDATEDDATA"
+#    # now we update this data
+#    %w{calendar contacts notes}.each do |route|
+#        post "/data/update/#{route}", credentials
+#        post "/data/get/#{route}", credentials
+#        assert_equal 200 , last_response.status
+#        assert_equal credentials["data"], JSON.parse(last_response.body).first["data"]
+#    end
+#
+  end
+
+  def test_sync_data
+
+  end
+end
