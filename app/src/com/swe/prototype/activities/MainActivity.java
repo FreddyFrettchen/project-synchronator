631d7eb8daf8dbf2f514f15af9c447d2f5f42fe4
runnerle <Runnerle@googlemail.com>
2013-12-11 18:13:37 +0000

    - weitere servertests - login vorbereitet - buttons und inputfeldern in main activity sinnvolle ids gegeben

--- app/src/com/swe/prototype/activities/MainActivity.java
+++ app/src/com/swe/prototype/activities/MainActivity.java
@@ -1,19 +1,18 @@
 package com.swe.prototype.activities;
 
 import com.swe.prototype.R;
-import com.swe.prototype.R.layout;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
 
 public class MainActivity extends BaseActivity {
+
+	private static final String TAG = "MainActivity";
 	
 	// test
 	@Override
@@ -21,16 +20,36 @@ public class MainActivity extends BaseActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		OnClickListener listnr = new OnClickListener() {
+		// buttons and listeners for login register
+		Button btn_register = (Button) findViewById(R.id.button_register);
+		btn_register.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				setContentView(R.layout.activity_accounts);
-				//Intent i = new Intent("AccountsActivity");
-				//startActivity(i);
+				switchToAccounts();
 			}
-		};
-		Button btn2 = (Button) findViewById(R.id.button2);
-		btn2.setOnClickListener(listnr);
+		});
+
+		Button btn_login = (Button) findViewById(R.id.button_login);
+		btn_login.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				doAuthentication();
+			}
+		});
+	}
+
+	private void switchToAccounts() {
+		Intent myIntent = new Intent(this, AccountsActivity.class);
+		startActivity(myIntent);
+	}
+
+	private void doAuthentication() {
+		String email = ((EditText)findViewById(R.id.input_email)).getText().toString();
+		String password = ((EditText)findViewById(R.id.input_password)).getText().toString();
+		/*Log.v(TAG, "email: " + email);
+		Log.v(TAG, "password: " + password);*/
+		
+		// login hier
 	}
 
 }
