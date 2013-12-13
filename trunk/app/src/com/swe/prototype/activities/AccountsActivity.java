4960a08e83761358b93f8cc44fc7a9ed1ef63ab1
Runnerle@googlemail.com <Runnerle@googlemail.com@6457d801-3e53-91b5-1c28-a1f0f58c1245>
2013-12-13 17:51:24 +0000

    eine register activity angelegt ( vorher wurde die accounts activity als register benutzt )

--- trunk/app/src/com/swe/prototype/activities/AccountsActivity.java
+++ trunk/app/src/com/swe/prototype/activities/AccountsActivity.java
@@ -34,103 +34,10 @@ import com.swe.prototype.net.server.Server.AuthenticateUserTask;
 public class AccountsActivity extends BaseActivity {
 
 	private static final String TAG = "AccountsActivity";
-	ProgressDialog dialog;
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_accounts);
-
-		// buttons and listeners for register button
-		Button btn_register = (Button) findViewById(R.id.register_button_register);
-		btn_register.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				doRegistration();
-			}
-		});
-	}
-
-	private void initializeDialog() {
-		dialog = ProgressDialog.show(AccountsActivity.this, "",
-				"Bitte einen moment geduld...", true);
-		dialog.show();
-	}
-
-	private void alertPasswordsNotMatching() {
-		AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
-
-		dlgAlert.setTitle("Registrierung fehlgeschlagen");
-		dlgAlert.setMessage("Die Passwörter stimmen leider nicht überein.");
-		dlgAlert.setPositiveButton("OK", null);
-		dlgAlert.setCancelable(true);
-		dlgAlert.create().show();
-	}
-
-	/**
-	 * returns the content of a editText by id.
-	 * 
-	 * @param id
-	 * @return
-	 */
-	private String getField(int id) {
-		return ((EditText) findViewById(id)).getText().toString();
-	}
-
-	private void doRegistration() {
-		initializeDialog();
-		String email = getField(R.id.register_input_email);
-		String password = getField(R.id.register_input_password);
-		String password2 = getField(R.id.register_input_password_rep);
-
-		if (!password.equals(password2)) {
-			alertPasswordsNotMatching();
-			return;
-		}
-
-		if (hasInternetConnection()) {
-			Server server = new Server();
-			server.new RegisterUserTask() {
-				@Override
-				protected void onPostExecute(Boolean result) {
-					dialog.dismiss();
-					if (result)
-						postRegister();
-					else
-						registerFailed();
-				}
-			}.execute(SERVER, email, password);
-		} else {
-			Log.i(TAG, "No internet");
-		}
-	}
-
-	private void registerFailed() {
-		AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
-
-		dlgAlert.setMessage("E-Mail Adresse nicht valide oder schon vergeben.");
-		dlgAlert.setTitle("Registrierung fehlgeschlagen");
-		dlgAlert.setPositiveButton("OK", null);
-		dlgAlert.setCancelable(true);
-		dlgAlert.create().show();
-	}
-
-	private void openLogin() {
-		startActivity(new Intent(this, MainActivity.class));
-		finish();
-	}
-
-	private void postRegister() {
-		AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
-
-		dlgAlert.setMessage("Nachdem Ihr Account vom Admin freigeschaltet wurde, können Sie sich einloggen.");
-		dlgAlert.setTitle("Registrierung erfolgreich");
-		dlgAlert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) {
-				openLogin();
-			}
-		});
-		dlgAlert.setCancelable(true);
-		dlgAlert.create().show();
 	}
 }
