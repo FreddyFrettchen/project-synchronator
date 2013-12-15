be7f2b555b081a1b7af95557c2ad16970b89b66c
Runnerle@googlemail.com <Runnerle@googlemail.com@6457d801-3e53-91b5-1c28-a1f0f58c1245>
2013-12-15 23:26:27 +0000

    server: email validation

--- trunk/app/src/com/swe/prototype/net/server/Server.java
+++ trunk/app/src/com/swe/prototype/net/server/Server.java
@@ -11,13 +11,23 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.AlgorithmParameterSpec;
+import java.security.spec.KeySpec;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
 import android.os.AsyncTask;
+import android.util.Base64;
 import android.util.Log;
 
 /**
@@ -58,7 +68,7 @@ public class Server {
 	 * @author batman
 	 * 
 	 */
-	public class RegisterUserTask extends AsyncUserTask{
+	public class RegisterUserTask extends AsyncUserTask {
 		protected Boolean doInBackground(String... params) {
 			String serverurl = params[0];
 			String username = params[1];
@@ -97,12 +107,10 @@ public class Server {
 			return false;
 		}
 	}
-	
+
 	/**
-	 * get data from useraccount on the server
-	 * to the app.
-	 * Task returns a list of entries of specified Type
-	 * given in param[2]
+	 * get data from useraccount on the server to the app. Task returns a list
+	 * of entries of specified Type given in param[2]
 	 * 
 	 * @author batman
 	 */
@@ -124,6 +132,81 @@ public class Server {
 				e.printStackTrace();
 			}
 			return null;
+		}
+	}
+
+	public class Security {
+
+		Cipher ecipher;
+		Cipher dcipher;
+		byte[] salt = new byte[8];
+		int iterationCount = 200;
+
+		public Security(String passPhrase) {
+			try {
+				// generate a random salt
+				SecureRandom random = new SecureRandom();
+				random.nextBytes(salt);
+
+				// Create the key
+				KeySpec keySpec = new PBEKeySpec(passPhrase.toCharArray(),
+						salt, iterationCount);
+				SecretKey key = SecretKeyFactory.getInstance(
+						"PBEWithSHA256And256BitAES-CBC-BC").generateSecret(
+						keySpec);
+				ecipher = Cipher.getInstance(key.getAlgorithm());
+				dcipher = Cipher.getInstance(key.getAlgorithm());
+
+				// Prepare the parameter to the ciphers
+				AlgorithmParameterSpec paramSpec = new PBEParameterSpec(salt,
+						iterationCount);
+
+				// Create the ciphers
+				ecipher.init(Cipher.ENCRYPT_MODE, key, paramSpec);
+				dcipher.init(Cipher.DECRYPT_MODE, key, paramSpec);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		public String encrypt(String str) {
+			try {
+				// Encode the string into bytes using utf-8
+				byte[] utf8 = str.getBytes("UTF8");
+
+				// Encrypt
+				byte[] enc = ecipher.doFinal(utf8);
+
+				// Encode bytes to base64 to get a string
+				return Base64.encodeToString(enc, Base64.DEFAULT);
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+
+		public String decrypt(String str) {
+			try {
+				// Decode base64 to get bytes
+				byte[] dec = Base64.decode(str, Base64.DEFAULT);
+
+				// Decrypt
+				byte[] utf8 = dcipher.doFinal(dec);
+
+				// Decode using utf-8
+				return new String(utf8, "UTF8");
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+
+		public int getIterationCount() {
+			return iterationCount;
+		}
+
+		public String getSalt() {
+			return Base64.encodeToString(salt, Base64.DEFAULT);
 		}
 	}
 }
