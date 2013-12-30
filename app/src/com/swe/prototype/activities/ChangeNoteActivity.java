41c15a40f277e307a6a0e8560c6179b5eff89a3c
danyossel <danyossel@gmail.com>
2013-12-30 13:40:02 +0000

    die 3 checkboxen: Synchronator, Google, Exchange hinzugefügt, um auszuwählen, wo man die notiz speichern kann. save und cancel button verschoben, (wie im Lastenheft vorgegeben)

--- app/src/com/swe/prototype/activities/ChangeNoteActivity.java
+++ app/src/com/swe/prototype/activities/ChangeNoteActivity.java
@@ -5,6 +5,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,14 +16,24 @@ public class ChangeNoteActivity extends BaseActivity {
 	
 	private static final String TAG = "NoteActivity";
 	private int changeNotePos;
+	EditText noteText;
+	CheckBox synchronatorCheckbox;
+	CheckBox googleCheckbox;
+	CheckBox exchangeCheckbox;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Bundle b = getIntent().getExtras();
+		Bundle b = getIntent().getExtras(); // hier muss ich auch noch informationen mitschicken, von welchem account die daten kommen.
 		changeNotePos = b.getInt("pos");
 		setContentView(R.layout.activity_changenote);
+		// hier dann entsprechend setzen, von wo die daten kommen.
+		// hier dann checkboxen, ausblebenden, je nachdem ob der user den entsprechenden account hinzugefügt hat.
+		synchronatorCheckbox = (CheckBox) findViewById(R.id.checkBox_synchronator);
+		googleCheckbox = (CheckBox) findViewById(R.id.checkBox_google);
+		exchangeCheckbox = (CheckBox) findViewById(R.id.checkBox_exchange);
+		
 		TextView noteTitle = (TextView)findViewById(R.id.textview_notetitle_changenote);
-		EditText noteText = (EditText)findViewById(R.id.edittext_changenote);
+		noteText = (EditText)findViewById(R.id.edittext_changenote);
 		noteTitle.setText(this.getNoteTitle(changeNotePos));
 		noteText.setText(this.getNoteText(changeNotePos));
 	}
@@ -38,7 +49,17 @@ public class ChangeNoteActivity extends BaseActivity {
 	
 	public void onClickSave(View v){
 		System.out.println("Save wurde gedrückt!");
+		System.out.println("Text: "+noteText.getText());
 		// hier muss dann der string aus dem edit text geholt werden und an changeNotePos in die datenbank geschrieben werden
+		if(synchronatorCheckbox.isChecked()){
+			System.out.println("Synchronator is checked!");
+		}
+		if(googleCheckbox.isChecked()){
+			System.out.println("google is checked!");
+		}
+		if(exchangeCheckbox.isChecked()){
+			System.out.println("Exchange is checked!");
+		}
 	}
 	public void onClickCancel(View v){
 		this.finish();
@@ -48,6 +69,8 @@ public class ChangeNoteActivity extends BaseActivity {
 		String l = "Nicht vergessen hier den Text aus dem datenmodel zu holen\nNicht vergessen hier den Text aus dem datenmodel zu holen\nNicht vergessen hier den Text aus dem datenmodel zu holen\n\tNicht vergessen hier den Text aus dem datenmodel zu holen\nPosition aus der liste" +pos;
 		return l;
 	}
+	
+	
 	private String getNoteTitle(int pos){
 		String l = "Hole Titel aus Datenbestand: "+pos;
 		return l;
