238303ee8add64b1502afeffe0afb4fb88fe6b01
danyossel@gmail.com <danyossel@gmail.com@6457d801-3e53-91b5-1c28-a1f0f58c1245>
2014-01-01 19:25:42 +0000

    -create new Note implementiert -in actionbar + hinzugefügt (jede activity sollte nun addClicked überschrieben)

--- trunk/app/src/com/swe/prototype/activities/ChangeNoteActivity.java
+++ trunk/app/src/com/swe/prototype/activities/ChangeNoteActivity.java
@@ -1,5 +1,6 @@
 package com.swe.prototype.activities;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,25 +19,55 @@ public class ChangeNoteActivity extends BaseActivity {
 	private static final String TAG = "NoteActivity";
 	private int changeNotePos;
 	EditText noteText;
+	EditText noteTitleEditText;
+	TextView noteTitle;
 	CheckBox synchronatorCheckbox;
 	CheckBox googleCheckbox;
 	CheckBox exchangeCheckbox;
+	boolean createNewNote = false;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Bundle b = getIntent().getExtras(); // hier muss ich auch noch informationen mitschicken, von welchem account die daten kommen.
-		changeNotePos = b.getInt("pos");
 		setContentView(R.layout.activity_changenote);
-		// hier dann entsprechend setzen, von wo die daten kommen.
-		// hier dann checkboxen, ausblebenden, je nachdem ob der user den entsprechenden account hinzugefuegt hat.
+		noteTitle = (TextView)findViewById(R.id.textview_notetitle_changenote);
+		noteText = (EditText)findViewById(R.id.edittext_changenote);
+		noteTitleEditText = (EditText)findViewById(R.id.edittext_notetitle_changenote); // hier könnte man versuchen das ding einzeilig zu machen
 		synchronatorCheckbox = (CheckBox) findViewById(R.id.checkBox_synchronator);
 		googleCheckbox = (CheckBox) findViewById(R.id.checkBox_google);
 		exchangeCheckbox = (CheckBox) findViewById(R.id.checkBox_exchange);
 		
-		TextView noteTitle = (TextView)findViewById(R.id.textview_notetitle_changenote);
-		noteText = (EditText)findViewById(R.id.edittext_changenote);
-		noteTitle.setText(this.getNoteTitle(changeNotePos));
-		noteText.setText(this.getNoteText(changeNotePos));
+		Bundle b = getIntent().getExtras(); 
+		createNewNote = b.getBoolean("createNewNote");
+		// da wir diese activity zum verändern und zum neuen Note erzeugen verwenden müssen wir beim onCreate und beim saven unterscheiden
+		ActionBar actionbar = getActionBar();
+		if(createNewNote){
+			actionbar.setTitle("Create New Note");
+			noteTitle.setText("Title: ");
+			
+			// checkboxen ausblenden falls user den account nicht hat.
+			googleCheckbox.setVisibility(View.INVISIBLE);
+			exchangeCheckbox.setVisibility(View.INVISIBLE);
+		}
+		else{
+			actionbar.setTitle("Change Note");
+			noteTitleEditText.setVisibility(View.INVISIBLE);
+			changeNotePos = b.getInt("pos");
+			noteTitle.setText(this.getNoteTitle(changeNotePos));
+			noteText.setText(this.getNoteText(changeNotePos));
+			//b.getInt // hier muss ich auch noch informationen mitschicken, von welchem account die daten kommen.
+			// hier dann entsprechend setzen, von wo die daten kommen.
+			// hier dann checkboxen, ausblebenden, je nachdem ob der user den entsprechenden account hinzugefuegt hat.
+			googleCheckbox.setVisibility(View.INVISIBLE);
+			exchangeCheckbox.setVisibility(View.INVISIBLE);
+		}
+		
+		
+
+		
+		
+		
+		
+		
 	}
 	
 	/*
@@ -51,6 +82,15 @@ public class ChangeNoteActivity extends BaseActivity {
 	public void onClickSave(View v){
 		System.out.println("Save wurde gedrueckt!");
 		System.out.println("Text: "+noteText.getText());
+		
+		if(this.createNewNote){
+			// neue anlegen
+			
+		}
+		else{
+			// geänderte notiz speichern
+			
+		}
 		// hier muss dann der string aus dem edit text geholt werden und an changeNotePos in die datenbank geschrieben werden
 		if(synchronatorCheckbox.isChecked()){
 			System.out.println("Synchronator is checked!");
