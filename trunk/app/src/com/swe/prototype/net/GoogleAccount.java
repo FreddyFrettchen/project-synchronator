5b2eb0e7249cc082ff5ba4802d553afedefe7317
bruegge.t.1990@gmail.com <bruegge.t.1990@gmail.com@6457d801-3e53-91b5-1c28-a1f0f58c1245>
2014-01-09 17:52:42 +0000

    GoogleKontakte eingesetzt

--- trunk/app/src/com/swe/prototype/net/GoogleAccount.java
+++ trunk/app/src/com/swe/prototype/net/GoogleAccount.java
@@ -1,15 +1,36 @@
 package com.swe.prototype.net;
 
+import java.net.URL;
+
 import android.content.Context;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 
+import com.google.gdata.client.contacts.ContactsService;
+import com.google.gdata.data.PlainTextConstruct;
+import com.google.gdata.data.contacts.ContactEntry;
+import com.google.gdata.data.extensions.City;
+import com.google.gdata.data.extensions.Country;
+import com.google.gdata.data.extensions.Email;
+import com.google.gdata.data.extensions.FamilyName;
+import com.google.gdata.data.extensions.FormattedAddress;
+import com.google.gdata.data.extensions.FullName;
+import com.google.gdata.data.extensions.GivenName;
+import com.google.gdata.data.extensions.Im;
+import com.google.gdata.data.extensions.Name;
+import com.google.gdata.data.extensions.PhoneNumber;
+import com.google.gdata.data.extensions.PostCode;
+import com.google.gdata.data.extensions.Region;
+import com.google.gdata.data.extensions.Street;
+import com.google.gdata.data.extensions.StructuredPostalAddress;
 import com.swe.prototype.models.AccountBase;
 import com.swe.prototype.models.CalendarEntry;
 import com.swe.prototype.models.Contact;
 import com.swe.prototype.models.Note;
 
 public class GoogleAccount extends AccountBase {
+	private final static String TAG = "GoogleKalender";
 
 	public GoogleAccount(Context context, int account_id, int refresh_time_sec,
 			String username, String password) {
@@ -19,7 +40,7 @@ public class GoogleAccount extends AccountBase {
 	@Override
 	public void synchronize() {
 		// TODO Auto-generated method stub
-
+		Log.i(TAG, "Synchronize wurde aufgerufen!");
 	}
 
 	@Override
@@ -33,6 +54,56 @@ public class GoogleAccount extends AccountBase {
 			String phonenumber, String email) {
 		// TODO Auto-generated method stub
 
+		Log.i(TAG, "CreateContact!!!!!!!!!!!!!!");
+		System.out.println("CreateContact!!!!!!!!!!!!");
+		try
+    	{
+    		ContactsService myService = new ContactsService("<var>YOUR_APPLICATION_NAME</var>");
+    		myService.setUserCredentials(this.username, this.password);
+    		
+	    	// Create the entry to insert.
+	    	ContactEntry contact = new ContactEntry();
+	    	// Set the contact's name.
+	    	Name name = new Name();
+	    	final String NO_YOMI = null;
+	    	name.setFullName(new FullName(lastname +" "+ firstname, NO_YOMI));
+	    	name.setGivenName(new GivenName(firstname, NO_YOMI));
+	    	name.setFamilyName(new FamilyName(lastname, NO_YOMI));
+	    	contact.setName(name);
+	    	//contact.setContent(new PlainTextConstruct("Notes"));
+	    	// Set contact's e-mail addresses.
+	    	Email primaryMail = new Email();
+	    	primaryMail.setAddress(email);
+	    	primaryMail.setDisplayName(lastname+" "+firstname);
+	    	primaryMail.setRel("http://schemas.google.com/g/2005#home");
+	    	primaryMail.setPrimary(true);
+	    	contact.addEmailAddress(primaryMail);
+	    	// Set contact's phone numbers.
+	    	PhoneNumber primaryPhoneNumber = new PhoneNumber();
+	    	primaryPhoneNumber.setPhoneNumber("(206)555-1212");
+	    	primaryPhoneNumber.setRel("http://schemas.google.com/g/2005#work");
+	    	primaryPhoneNumber.setPrimary(true);
+	    	contact.addPhoneNumber(primaryPhoneNumber);
+	    	// Set contact's postal address.
+	    	/*StructuredPostalAddress postalAddress = new StructuredPostalAddress();
+	    	postalAddress.setStreet(new Street("1600 Amphitheatre Pkwy"));
+	    	postalAddress.setCity(new City("Mountain View"));
+	    	postalAddress.setRegion(new Region("CA"));
+	    	postalAddress.setPostcode(new PostCode("94043"));
+	    	postalAddress.setCountry(new Country("US", "United States"));
+	    	postalAddress.setFormattedAddress(new FormattedAddress("1600 Amphitheatre Pkwy Mountain View"));
+	    	postalAddress.setRel("http://schemas.google.com/g/2005#work");
+	    	postalAddress.setPrimary(true);
+	    	contact.addStructuredPostalAddress(postalAddress);
+	    	*/// Ask the service to insert the new entry
+	    	URL postUrl = new URL("https://www.google.com/m8/feeds/contacts/default/full");
+	    	ContactEntry createdContact = myService.insert(postUrl, contact);
+	    	System.out.println("Contact's ID: " + createdContact.getId());
+    	}
+    	catch(Exception e)
+    	{
+    		System.out.println(e);
+    	}
 	}
 
 	@Override
