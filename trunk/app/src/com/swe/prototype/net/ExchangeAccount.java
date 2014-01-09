5f1b64e6a940336b29fff6df171b0579f9b6070a
bahos08@gmail.com <bahos08@gmail.com@6457d801-3e53-91b5-1c28-a1f0f58c1245>
2014-01-09 15:44:59 +0000

    ExchangeAccount.java delete funktionen und edit funktionen programmiert

--- trunk/app/src/com/swe/prototype/net/ExchangeAccount.java
+++ trunk/app/src/com/swe/prototype/net/ExchangeAccount.java
@@ -2,7 +2,10 @@ package com.swe.prototype.net;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import android.content.Context;
 import android.widget.ArrayAdapter;
@@ -21,10 +24,20 @@ import java.util.Date;
 
 import com.independentsoft.exchange.Appointment;
 import com.independentsoft.exchange.Body;
+import com.independentsoft.exchange.ContactPropertyPath;
+import com.independentsoft.exchange.DeleteType;
 import com.independentsoft.exchange.FileAsMapping;
+import com.independentsoft.exchange.IsEqualTo;
+import com.independentsoft.exchange.IsLessThan;
 import com.independentsoft.exchange.ItemId;
+import com.independentsoft.exchange.ItemShape;
+import com.independentsoft.exchange.MessagePropertyPath;
 import com.independentsoft.exchange.Note;
 import com.independentsoft.exchange.NoteColor;
+import com.independentsoft.exchange.NotePropertyPath;
+import com.independentsoft.exchange.Property;
+import com.independentsoft.exchange.Response;
+import com.independentsoft.exchange.ShapeType;
 import com.swe.prototype.models.AccountBase;
 import com.swe.prototype.models.CalendarEntry;
 import com.swe.prototype.models.Contact;
@@ -120,41 +133,6 @@ public class ExchangeAccount extends AccountBase {
 	}
 
 	@Override
-	public void editContact(Context context, Contact c) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void editNote(Context context, com.swe.prototype.models.Note n) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void editCalendarEntry(Context context, CalendarEntry ce) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteContact(Contact c) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteNote(com.swe.prototype.models.Note n) {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void deleteCalendarEntry(CalendarEntry ce) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
 	public BaseAdapter getCalendarAdapter(Context context, int layout_id) {
 		ArrayAdapter<CalendarEntry> adapter = new ArrayAdapter<CalendarEntry>(
 				context, layout_id);
@@ -180,6 +158,157 @@ public class ExchangeAccount extends AccountBase {
 	}
 
 	@Override
+	public void editContact(Context context, Contact c) {
+		// TODO Auto-generated method stub
+		try
+        {
+			Service service = new Service(
+					"https://mail.fh-aachen.de/EWS/exchange.asmx",
+					"bd8299s@ad.fh-aachen.de", "password");
+
+
+            IsEqualTo restriction = new IsEqualTo(ContactPropertyPath.EMAIL1_ADDRESS, c.getEmail());
+
+            FindItemResponse response = service.findItem(StandardFolder.CONTACTS, restriction);
+
+            for (int i = 0; i < response.getItems().size(); i++)
+            {
+                if (response.getItems().get(i) instanceof com.independentsoft.exchange.Contact)
+                {
+                    ItemId itemId = response.getItems().get(i).getItemId();
+
+                    Property businessPhonePropertyFN = new Property(ContactPropertyPath.GIVEN_NAME, c.getFirstName());
+                    Property businessPhonePropertyLN = new Property(ContactPropertyPath.SURNAME, c.getLastName());
+                    Property businessPhonePropertyBP = new Property(ContactPropertyPath.BUSINESS_PHONE, c.getPhoneumber());
+
+                    itemId = service.updateItem(itemId, businessPhonePropertyFN);
+                    itemId = service.updateItem(itemId, businessPhonePropertyLN);
+                    itemId = service.updateItem(itemId, businessPhonePropertyBP);
+                }
+            }
+        }
+        catch (ServiceException e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println(e.getXmlMessage());
+
+            e.printStackTrace();
+        }
+    }
+	
+	@Override
+	public void editNote(Context context, com.swe.prototype.models.Note n) {
+		// TODO Auto-generated method stub
+		try
+        {
+			Service service = new Service(
+					"https://mail.fh-aachen.de/EWS/exchange.asmx",
+					"bd8299s@ad.fh-aachen.de", "password");
+
+
+            IsEqualTo restriction = new IsEqualTo(NotePropertyPath.SUBJECT, n.getTitle());
+
+            FindItemResponse response = service.findItem(StandardFolder.NOTES, restriction);
+
+            for (int i = 0; i < response.getItems().size(); i++)
+            {
+                if (response.getItems().get(i) instanceof com.independentsoft.exchange.Note)
+                {
+                    ItemId itemId = response.getItems().get(i).getItemId();
+
+                    Property noteProperty = new Property(NotePropertyPath.BODY, n.getNote());
+
+                    itemId = service.updateItem(itemId, noteProperty);
+                }
+            }
+        }
+        catch (ServiceException e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println(e.getXmlMessage());
+
+            e.printStackTrace();
+        }
+
+	}
+
+	@Override
+	public void editCalendarEntry(Context context, CalendarEntry ce) {
+		// TODO Auto-generated method stub
+		}
+
+	@Override
+	public void deleteContact(Contact c) {
+		 try
+	        {
+			 String vergleichsString = "";
+	        	Service service = new Service(
+						"https://mail.fh-aachen.de/EWS/exchange.asmx",
+						"bd8299s@ad.fh-aachen.de", "password");
+
+	            FindItemResponse contactItems = service.findItem(StandardFolder.CONTACTS);
+	         
+	            //falls vorname oder nachname nicht angegeben ist, wird das leerzeichen nicht mit abgefragt
+	            if(!(c.getFirstName().isEmpty() || c.getLastName().isEmpty())){
+	            	vergleichsString = c.getFirstName()+c.getLastName();
+	            }
+	            
+	            for (int i = 0; i < contactItems.getItems().size(); i++)
+	            {
+	            	if(contactItems.getItems().get(i).getSubject().equals(vergleichsString)){
+	            	System.out.println(contactItems.getItems().get(i).getSubject());
+	                Response response = service.deleteItem(contactItems.getItems().get(i).getItemId(), DeleteType.HARD_DELETE);
+	            	}
+	            	else{
+	            		System.out.println("Löschen nicht Erfolgreich: " + contactItems.getItems().get(i));
+	            	}
+	            }
+	        }
+	        catch (ServiceException e)
+	        {
+	            System.out.println(e.getMessage());
+	            System.out.println(e.getXmlMessage());
+
+	            e.printStackTrace();
+	        }
+	}
+
+	@Override
+	public void deleteNote(com.swe.prototype.models.Note n) {
+		// TODO Auto-generated method stub
+		try
+        {
+        	Service service = new Service(
+					"https://mail.fh-aachen.de/EWS/exchange.asmx",
+					"bd8299s@ad.fh-aachen.de", "password");
+
+        	IsEqualTo restriction = new IsEqualTo(NotePropertyPath.SUBJECT, n.getTitle());
+        	
+            FindItemResponse notesItems = service.findItem(StandardFolder.NOTES, restriction);
+         
+            
+            for (int i = 0; i < notesItems.getItems().size(); i++)
+            {
+            	Response response = service.deleteItem(notesItems.getItems().get(i).getItemId(), DeleteType.HARD_DELETE);
+            }
+        }
+        catch (ServiceException e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println(e.getXmlMessage());
+
+            e.printStackTrace();
+        }
+
+	}
+
+	@Override
+	public void deleteCalendarEntry(CalendarEntry ce) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
 	public void createNote(String title, String text) {
 		try {
 			Service service = new Service(
@@ -188,8 +317,8 @@ public class ExchangeAccount extends AccountBase {
 													// "password");
 
 			Note note = new Note();
-			note.setSubject("My test note");
-			note.setBody(new Body("My test note"));
+			note.setSubject(title);
+			note.setBody(new Body(text));
 			note.setColor(NoteColor.GREEN);
 			note.setIconColor(NoteColor.GREEN);
 			note.setHeight(200);
@@ -217,17 +346,18 @@ public class ExchangeAccount extends AccountBase {
 
 			SimpleDateFormat dateFormat = new SimpleDateFormat(
 					"yyyy-MM-dd HH:mm:ss");
-			Date _startTime = dateFormat.parse("2014-02-25 16:00:00");
-			Date _endTime = dateFormat.parse("2014-02-25 18:00:00");
+			Date _startTime = dateFormat.parse(startDate + startTime);//"2014-02-25 16:00:00");
+			Date _endTime = dateFormat.parse(endDate + endTime);//"2014-02-25 18:00:00");
 
 			Appointment appointment = new Appointment();
-			appointment.setSubject("Test");
-			appointment.setBody(new Body("Body text"));
+			appointment.setSubject("");
+			appointment.setBody(new Body(description));
 			appointment.setStartTime(_startTime);
 			appointment.setEndTime(_endTime);
-			appointment.setLocation("My Office");
+			appointment.setLocation(description);
 			appointment.setReminderIsSet(true);
 			appointment.setReminderMinutesBeforeStart(30);
+			//appointment.setReminderNextTime(repeat); Reminder noch finden und repeat??? was übergibt es mir
 
 			ItemId itemId = service.createItem(appointment);
 		} catch (ServiceException e) {
@@ -240,5 +370,5 @@ public class ExchangeAccount extends AccountBase {
 		}
 
 	}
-
+	
 }
