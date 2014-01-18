7aeb9e4f9ef4b48030d954b361af918b6a1250c4
danyossel@gmail.com <danyossel@gmail.com@6457d801-3e53-91b5-1c28-a1f0f58c1245>
2014-01-18 21:13:03 +0000

    dayview

---
+++ trunk/app/src/com/swe/prototype/helpers/RectangleCalendarEntry.java
@@ -0,0 +1,29 @@
+package com.swe.prototype.helpers;
+
+import com.swe.prototype.models.CalendarEntry;
+
+/**
+ * @author Dany Brossel
+ *
+ * Wird in der DayView zur Koordinatenspeicherung der einzelnen CalendarEntys benutzt
+ */
+public class RectangleCalendarEntry {
+	
+	public CalendarEntry calEntry;
+	//pixel values
+	public int x1;
+	public int y1;
+	public int x2;
+	public int y2;
+	public int h;
+	public int w;
+	public RectangleCalendarEntry(CalendarEntry e, int x1, int y1, int x2, int y2) {
+		this.calEntry = e;
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		this.h = y2-y1;
+		this.w=x2-x1;
+	}
+}
