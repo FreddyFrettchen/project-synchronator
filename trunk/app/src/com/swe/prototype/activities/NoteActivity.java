12e8f5462f961448e52c6e239326a745f3f3772d
freddyfrettchen86@gmail.com <freddyfrettchen86@gmail.com@6457d801-3e53-91b5-1c28-a1f0f58c1245>
2014-01-18 20:27:17 +0000

    -Ansichten angepasst -Note activities/NoteActivity.java von BaseActivity abgeleitet (Wegen Header)

--- trunk/app/src/com/swe/prototype/activities/NoteActivity.java
+++ trunk/app/src/com/swe/prototype/activities/NoteActivity.java
@@ -2,12 +2,11 @@ package com.swe.prototype.activities;
 
 import com.swe.prototype.R;
 
-import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class NoteActivity extends Activity {
+public class NoteActivity extends BaseActivity {
 	private static final String TAG = "NoteActivity";
 	ProgressDialog dialog;
 	
